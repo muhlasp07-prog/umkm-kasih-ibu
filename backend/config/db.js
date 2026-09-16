a87333130306fbb6de45d031105e7ef86cdcf799
muhlasp07-prog <muhlasp07@gmail.com>
2026-09-16 20:07:49 +0800

    gunakan environment variable untuk koneksi database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     dateStrings: true,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'umkm_bima',
+    host: process.env.MYSQLHOST || 'localhost',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || '',
+    database: process.env.MYSQLDATABASE || 'umkm_bima',
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
